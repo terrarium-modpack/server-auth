429e39911416bb46fef09b76b6312b29769dc1f0
amyavi <144570677+amyavi@users.noreply.github.com>
2025-04-16 18:53:17 -0300

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.0"
-    id("fabric-loom") version "1.7-SNAPSHOT"
+    kotlin("jvm") version "2.1.20"
+    id("fabric-loom") version "1.10-SNAPSHOT"
 }
 
 version = "1.0.5-SNAPSHOT"
@@ -9,13 +9,13 @@ group = "dev.optimistic"
 dependencies {
     minecraft("com.mojang:minecraft:1.20.1")
     mappings("net.fabricmc:yarn:1.20.1+build.10:v2")
-    modImplementation("net.fabricmc:fabric-loader:0.15.11")
+    modImplementation("net.fabricmc:fabric-loader:0.16.14")
 
     setOf("fabric-api-base", "fabric-command-api-v2").forEach {
-        modImplementation(fabricApi.module(it, "0.92.2+1.20.1"))
+        modImplementation(fabricApi.module(it, "0.92.5+1.20.1"))
     }
 
-    modImplementation("net.fabricmc:fabric-language-kotlin:1.11.0+kotlin.2.0.0")
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.13.2+kotlin.2.1.20")
 }
 
 java {
